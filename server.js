2e16864f9aa5dacb3c66ff07e0d840280824dc1b
Mrudulvbinu <mrudulbinu@gmail.com>
2025-03-29 20:42:10 +0530

    2.3rd

--- server.js
+++ server.js
@@ -44,10 +44,10 @@ const registeredhackathonRoutes = require("./routes/hackathonregistrationroutes"
 app.use("/api/registeredhackathon", registeredhackathonRoutes);
 
 
-app.use(express.static(path.join(__dirname, "../dist"))); 
+app.use(express.static(path.join(__dirname,  "public"))); 
 
 app.get("*", (res) => {
-  res.sendFile(path.join(__dirname, "../dist/index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 
